6f5d9c65fb3060f80bd54236db0015772fe5a04f
gyoungseok <azd7777work@gmail.com>
2023-01-20 23:26:43 +0900

    makingTheScheduler_진행중

---
+++ partimeScheduler/알바스케줄러만들기/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  알바스케줄러만들기
+//
+//  Created by s ky on 2023/01/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
