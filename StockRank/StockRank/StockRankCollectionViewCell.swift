cd522edb82067fb5ba71ecf3026f022425f588a2
gyoungseok <azd7777work@gmail.com>
2023-02-02 00:22:53 +0900

    UICollectionView_리스트및그리드_실습

--- StockRank/StockRank/StockRankCollectionViewCell.swift
+++ StockRank/StockRank/StockRankCollectionViewCell.swift
@@ -9,4 +9,41 @@ import UIKit
 
 class StockRankCollectionViewCell: UICollectionViewCell {
     
+    // uicomponent를 연결하자
+    // uicomponent에 데이터를 업데이트 하는 코드를 넣자
+    
+    @IBOutlet weak var rankLabel: UILabel!
+    @IBOutlet weak var companyIconImageView: UIImageView!
+    @IBOutlet weak var companyNameLabel: UILabel!
+    @IBOutlet weak var companyPriceLabel: UILabel!
+    @IBOutlet weak var diffLabel: UILabel!
+    
+    func configure(_ stock: StockModel) {
+        rankLabel.text = "\(stock.rank)"
+        companyIconImageView.image = UIImage(named: stock.imageName)
+        companyNameLabel.text = stock.name
+        companyPriceLabel.text = "\(convertToCurrencyFormat(price: stock.price)) 원"
+        
+        
+        // 등락폭 색깔 방법1 (if문)
+//        let color: UIColor
+//        if stock.diff > 0 {
+//            color = UIColor.systemRed
+//        } else {
+//            color = UIColor.systemBlue
+//        }
+        
+        // 등락폭 색깔 방법2 (삼항연산자)
+        diffLabel.textColor = stock.diff > 0 ? UIColor.systemRed : UIColor.systemBlue
+        diffLabel.text = "\(stock.diff) %"
+    }
+    
+    func convertToCurrencyFormat(price: Int) -> String {
+        let numberformatter = NumberFormatter()
+        numberformatter.numberStyle = .decimal
+        numberformatter.maximumFractionDigits = 0
+        let result =  numberformatter.string(from: NSNumber(value: price)) ?? ""
+        return result
+    }
+    
 }
--- StockRank/StockRank/StockRankViewController.swift
+++ StockRank/StockRank/StockRankViewController.swift
@@ -43,14 +43,27 @@ extension StockRankViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
         // 재사용가능한 cell을 가지고 오겠다. 구분자 , 순서
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "StockRankCollectionViewCell", for: indexPath)
-        return cell 
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "StockRankCollectionViewCell", for: indexPath) as? StockRankCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        let stock = stockList[indexPath.item]
+        cell.configure(stock)
+        return cell
         
         
-//        return UICollectionViewCell()
     }
 }
 
+// cell 들을 어떻게 배치할 것인지?
 extension StockRankViewController: UICollectionViewDelegateFlowLayout {
     
+    // 하나의 cell에 사이즈를 정하는 함수
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        // width == collectionView
+        // height = 80
+        
+        return CGSize(width: collectionView.bounds.width, height: 80)
+        
+    }
 }
