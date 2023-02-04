a5c3600cf65daa3e3529b6839948e7007571129d
gyoungseok <azd7777work@gmail.com>
2023-02-04 11:01:46 +0900

    diffableDataSource실습

---
+++ AppleFramework_DiffableDatasource/AppleFramework/FrameworkListViewController.swift
@@ -0,0 +1,75 @@
+//
+//  FrameworkListViewController.swift
+//  AppleFramework
+//
+//  Created by joonwon lee on 2022/04/24.
+//
+
+import UIKit
+
+class FrameworkListViewController: UIViewController {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    let list: [AppleFramework] = AppleFramework.list
+    
+    var dataSource: UICollectionViewDiffableDataSource<Section, Item>!
+    typealias Item = AppleFramework
+    enum Section {
+        case main
+    }
+    
+    // Data, Presentation, Layout
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        dataSource = UICollectionViewDiffableDataSource<Section, Item>(collectionView: collectionView, cellProvider: { collectionView, indexPath, Item in
+            
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "FrameworkCell", for: indexPath) as? FrameworkCell else {
+                return nil
+            }
+            
+            cell.configure(Item)
+            return cell
+        })
+        
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Item>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(list, toSection: .main)
+        dataSource.apply(snapshot)
+        
+        collectionView.collectionViewLayout = layout()
+        
+        // diffable datasource
+        // - presentation
+        
+        // snapshot
+        // - data
+        
+        // compositional layout
+        // - layout
+        
+    }
+    
+    private func layout() -> UICollectionViewCompositionalLayout{
+        
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.33), heightDimension: .fractionalWidth(1))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalWidth(0.33))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: 3)
+        let section = NSCollectionLayoutSection(group: group)
+        
+        section.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 16, bottom: 0, trailing: 16)
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        return layout
+    }
+}
+
+extension FrameworkListViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let framework = list[indexPath.item]
+        print(">>> selected: \(framework.name)")
+    }
+}
+
